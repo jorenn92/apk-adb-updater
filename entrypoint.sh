11870fc62b6f3c233fc89efcde745a049752f9da
Joren Vennekens <joren.vennekens@cipalschaubroeck.be>
2021-05-20 17:27:10 +0200

    Docker: Start adb daemon in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Start adb daemon
+/opt/aptoide-adb-updater/adb/linux/adb start-server
+
 echo "Starting aptoide_adb_updater" 
 python3 /opt/aptoide-adb-updater/aptoide_updater.py
