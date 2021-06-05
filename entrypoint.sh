fa8c0d9ac56fe76e9ea5be70768f819767b5a1bb
jorenn92 <jorenn92@gmail.com>
2021-06-05 15:41:50 +0200

    Empty logs on container boot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Starting apk_adb_updater" 
+echo "" > /opt/apk-adb-updater/logs/output.log
 python3 -u /opt/apk-adb-updater/apk_updater.py 2>&1 | tee /opt/apk-adb-updater/logs/output.log
