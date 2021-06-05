d72ceace06f63c80e0d4bce2049d0c6a3a8a5839
jorenn92 <jorenn92@gmail.com>
2021-06-05 15:39:10 +0200

    entrypoint add -u option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,3 @@
 
 echo "Starting apk_adb_updater" 
 python3 -u /opt/apk-adb-updater/apk_updater.py 2>&1 | tee /opt/apk-adb-updater/logs/output.log
-
-# docker run -d -v /usr/apps/apk-adb-updater/keys:/root/.android/ -v /usr/apps/apk-adb-updater/config:/opt/apk-adb-updater/config -v /usr/apps/apk-adb-updater/logs:/opt/apk-adb-updater/logs --net=host --name=apk-updater jorenn92/apk-adb-updater
